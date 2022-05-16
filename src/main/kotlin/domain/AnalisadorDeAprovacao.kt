54342f8fe162615be6c60610e27615dcd131a95a
Roberto Bolgheroni <robertobolgheroni@usp.br>
2022-05-15 22:42:13 -0300

    Add defineCriterio and fechaBoletim functions for AnalisadorDeAprovacao class

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,9 +1,22 @@
 package domain
 
 import domain.criterios.CriterioDeAprovacao
+import domain.Boletim
+import domain.BoletimFechado
 
 class AnalisadorDeAprovacao {
     
     val criterio: CriterioDeAprovacao
+
+    fun defineCriterio(criterio: CriterioDeAprovacao): Void {
+        this.criterio = criterio
+    }
+
+    fun fechaBoletim(boletim: Boletim): BoletimFechado {
+        val mediaFinal = this.criterio.mediaFinal(boletim)
+        val estaAprovado = this.criterio.estaAprovado(boletim)
+
+        return BoletimFechado(boletim.mediaEps, boletim.mediaMiniEPs, mediaFinal, estaAprovado)
+    }
 
 }
